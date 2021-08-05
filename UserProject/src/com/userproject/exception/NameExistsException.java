bdd60764e6c613c2f1a53a8e264c0b36d160c3cb
Jeeva R <jeevanadar1998@gmail.com>
2021-08-05 22:51:12 +0530

    UserDefined Exceptions are created and added

---
+++ UserProject/src/com/userproject/exception/NameExistsException.java
@@ -0,0 +1,20 @@
+package com.userproject.exception;
+
+public class NameExistsException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NameExistsException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NameExistsException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ UserProject/src/com/userproject/exception/TooShortException.java
@@ -0,0 +1,20 @@
+package com.userproject.exception;
+
+public class TooShortException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public TooShortException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public TooShortException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
